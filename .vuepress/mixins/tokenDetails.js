082307feeb5f779d4085eafe5dd984a36af6a386
Yuki Kurosawa <yuki@gakkougurashi.net>
2021-06-20 10:50:53 +0800

    Init Codes

---
+++ .vuepress/mixins/tokenDetails.js
@@ -0,0 +1,27 @@
+export default {
+  data () {
+    return {
+      tokenDetails: [
+        {
+          name: 'SimpleERC20',
+          version: '4.0.0',
+          standard: true,
+          verified: true,
+          detailed: true,
+          customizeDecimals: false,
+          supplyType: 'Fixed',
+          accessType: 'None',
+          transferType: 'Unstoppable',
+          mintable: false,
+          burnable: false,
+          erc1363: false,
+          tokenRecover: false,
+          removeCopyright: false,
+          originalPrice: 0,
+          price: 0,
+          gas: 1163434,
+        }
+      ],
+    };
+  },
+};
